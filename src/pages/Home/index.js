713164ccb6a2ca58c0d2cb6651d26dc4f3d7386d
► Azmi Adhani <azmi.adhani@ulm.ac.id>
2020-09-16 16:10:10 +0800

    Router Setted Up

---
+++ src/pages/Home/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {createStackNavigator} from '@react-navigation/stack';
+function HomeScreen() {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text>Home Screens</Text>
+    </View>
+  );
+}
+
+const Home = () => {
+  return (
+    <View>
+      <Text>BERANDA</Text>
+    </View>
+  );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({});
---
+++ src/pages/Pemilihan/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+const Pemilihan = () => {
+  return (
+    <View>
+      <Text>Pesanan</Text>
+    </View>
+  );
+};
+
+export default Pemilihan;
+
+const styles = StyleSheet.create({});
