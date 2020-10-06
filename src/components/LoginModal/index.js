6cc8be3e4728f43edf33a5701e5c6cb5e4b0dccf
► Azmi Adhani <azmi.adhani@ulm.ac.id>
2020-10-07 02:20:22 +0800

    post example

--- src/components/LoginModal/index.js
+++ src/components/LoginModal/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import {StyleSheet, Text, View, Image, Dimensions} from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Dimensions,
+  Button,
+  ActivityIndicator,
+  FlatList,
+} from 'react-native';
 import {MainLogo} from '../../assets';
 import {
   TextField,
   FilledTextField,
   OutlinedTextField,
 } from '@ubaids/react-native-material-textfield';
+import {SafeAreaInsetsContext} from 'react-native-safe-area-context';
+// import {username} from '../../Utils/globals';
 
 const LoginModal = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    // fetch('https://reqres.in/api/users/2')
+    //   .then((response) => response.json())
+    //   .then((json) => console.log(json));
+
+    // Call API Method POST
+    const dataForAPI = {
+      name: 'morpheus',
+      job: 'leader',
+    };
+    fetch('https://reqres.in/api/users/', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(dataForAPI),
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+  }, []);
   return (
     <View style={styles.container}>
+      <Button
+        title={username}
+        onPress={async () => {
+          console.log(await getMovies());
+          setData(getMovies());
+        }}
+      />
       <TextField
         label="Nomor Induk Mahasiswa"
+        defaultValue={username}
+        onChangeText={(text) => setUsername(text)}
         keyboardType="phone-pad"
         tintColor="rgba(0,0,0,0.4)"
         baseColor="rgba(0,0,0,0.4)"
@@ -19,6 +63,8 @@ const LoginModal = () => {
       />
       <TextField
         label="Kata Sandi"
+        value={password}
+        onChangeText={(text) => setPassword(text)}
         keyboardType="phone-pad"
         tintColor="rgba(0,0,0,0.4)"
         baseColor="rgba(0,0,0,0.4)"
