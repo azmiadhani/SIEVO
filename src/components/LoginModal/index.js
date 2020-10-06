5754467c6ccd463f259a745471b4c6794c7154e7
► Azmi Adhani <azmi.adhani@ulm.ac.id>
2020-10-07 03:39:32 +0800

    Ajax done

--- src/components/LoginModal/index.js
+++ src/components/LoginModal/index.js
@@ -17,59 +17,49 @@ import {
 } from '@ubaids/react-native-material-textfield';
 import {SafeAreaInsetsContext} from 'react-native-safe-area-context';
 // import {username} from '../../Utils/globals';
-
+function ajax(url) {
+  // resource : https://stackoverflow.com/questions/14220321/how-do-i-return-the-response-from-an-asynchronous-call
+  // `delay` returns a promise
+  return new Promise(function (resolve, reject) {
+    // Only `delay` is able to resolve or reject the promise
+    const fd = new FormData();
+    fd.append('userUsername', 'admin_kpu');
+    var xhr = new XMLHttpRequest();
+    xhr.open('post', url, true);
+    xhr.setRequestHeader('X-Requested-With', 'xmlhttprequest');
+    xhr.send(fd);
+    xhr.onload = function () {
+      resolve(this.response);
+    };
+    xhr.onerror = reject;
+  });
+}
 const LoginModal = () => {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
   const [text, setText] = useState('');
-  async function getAPIUser() {
-    // fetch('http://192.168.56.1/pemilu-m/user/api')
-    //   .then((response) => response.json())
-    //   .then((json) => console.log(json));
-    const dataForAPI = {
-      userUsername: 'admin_kpu',
-    };
-    const fd = new FormData();
-    fd.append('userUsername', 'admin_kpu');
-    fetch('http://192.168.56.1/pemilu-m/user/api', {
-      method: 'POST',
-      headers: {
-        'X-Requested-With': 'XMLHttpRequest',
-      },
-      body: fd,
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        setText(JSON.stringify(json));
-      });
-  }
-  async function getAPI() {
-    const dataForAPI = {
-      name: 'morpheus',
-      job: 'leader',
-    };
-    fetch('https://reqres.in/api/users/', {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify(dataForAPI),
-    })
-      .then((response) => response.json())
-      .then((json) => {
-        setUsername(json.name);
-      });
-  }
 
   return (
     <View style={styles.container}>
       <Text>{text}</Text>
       <Button
         title={username}
-        onPress={async () => {
-          getAPIUser();
+        onPress={() => {
+          // getAPIUser().then((res) => setText(res));
+          ajax('http://192.168.56.1/pemilu-m/user/api')
+            .then(function (res) {
+              // `delay` returns a promise
+              res = JSON.parse(res);
+              console.log(res);
+              setText(res.userUsername); // Log the value once it is resolved
+            })
+            .catch(function (res) {
+              // Or do something else if it is rejected
+              // (it would not happen in this example, since `reject` is not called).
+              console.log(res);
+            });
         }}
       />
       <TextField
