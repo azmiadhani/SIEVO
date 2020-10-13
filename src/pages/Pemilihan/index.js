4df3432981e2eae4de4c7152b0e4075393f0ee37
► Azmi Adhani <azmi.adhani@ulm.ac.id>
2020-10-13 10:40:02 +0800

    Pemilihan susun kamera

--- src/pages/Pemilihan/index.js
+++ src/pages/Pemilihan/index.js
@@ -1,23 +1,43 @@
 import React, {useEffect, useState, PureComponent} from 'react';
-import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Dimensions,
+  SafeAreaView,
+  s,
+} from 'react-native';
 import {checkLogin} from '../../Utils/asyncstorage';
 import {useNavigation} from '@react-navigation/native';
 import {RNCamera} from 'react-native-camera';
+import {
+  MainContent,
+  HField,
+  HButton,
+  HInput,
+  AsyncTest,
+} from '../../components';
+import {ScrollView} from 'react-native-gesture-handler';
 
 const Pemilihan = ({route}) => {
   const navigation = useNavigation();
   const [camera, setCamera] = useState('');
+  const [picture, setPicture] = useState('');
   const takePicture = async () => {
     // console.log(camera);
     if (camera) {
-      const options = {quality: 0.5, base64: true};
+      // const options = {quality: 0.5, base64: true};
+      const options = {quality: 0.5};
       const data = await camera.takePictureAsync(options);
+      setPicture(data);
+
       console.log(data.uri);
     }
   };
   useEffect(() => {
     if (route.params?.loaded) {
-      console.log('TAB - Beranda');
+      console.log('TAB - Pemilihan');
       checkLogin()
         .then(function (res) {
           if (res) {
@@ -33,56 +53,70 @@ const Pemilihan = ({route}) => {
   }, [route.params?.loaded]);
   return (
     <View style={styles.container}>
-      <RNCamera
-        ref={(ref) => {
-          setCamera(ref);
-        }}
-        style={styles.preview}
-        type={RNCamera.Constants.Type.front}
-        // flashMode={RNCamera.Constants.FlashMode.on}
-        faceDetectionMode={RNCamera.Constants.FaceDetection.Mode.accurate}
-        onFacesDetected={(data) => console.log(data)}
-        androidCameraPermissionOptions={{
-          title: 'Permission to use camera',
-          message: 'We need your permission to use your camera',
-          buttonPositive: 'Ok',
-          buttonNegative: 'Cancel',
-        }}
-        androidRecordAudioPermissionOptions={{
-          title: 'Permission to use audio recording',
-          message: 'We need your permission to use your audio',
-          buttonPositive: 'Ok',
-          buttonNegative: 'Cancel',
-        }}
-      />
+      {!picture && (
+        <RNCamera
+          ref={(ref) => {
+            setCamera(ref);
+          }}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.front}
+          // flashMode={RNCamera.Constants.FlashMode.on}
+          // faceDetectionMode={RNCamera.Constants.FaceDetection.Mode.accurate}
+          // onFacesDetected={(data) => console.log(data)}
+          androidCameraPermissionOptions={{
+            title: 'Permission to use camera',
+            message: 'We need your permission to use your camera',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          androidRecordAudioPermissionOptions={{
+            title: 'Permission to use audio recording',
+            message: 'We need your permission to use your audio',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+        />
+      )}
+
       <View
         style={{
           flex: 1,
-          flexDirection: 'row',
           justifyContent: 'center',
           backgroundColor: '#ffffff',
           borderRadius: 30,
         }}>
-        <TouchableOpacity
-          onPress={takePicture.bind(this)}
-          style={styles.capture}>
-          <Text style={{fontSize: 14, color: 'white'}}>AMBIL FOTO</Text>
-        </TouchableOpacity>
+        <ScrollView>
+          <View style={{paddingTop: 40, paddingBottom: 40}}>
+            <View style={{paddingRight: 30, paddingLeft: 30}}>
+              <HButton
+                label="AMBIL FOTO"
+                onPress={takePicture.bind(this)}
+                // console.log(refresh);
+              />
+              {/* <HInput label="Nomor Induk Mahasiswa" defaultValue="" />
+              <HInput label="Nomor Induk Mahasiswa" defaultValue="" /> */}
+            </View>
+          </View>
+        </ScrollView>
       </View>
     </View>
   );
 };
 
 export default Pemilihan;
-
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
 const styles = StyleSheet.create({
+  scrollView: {
+    height: windowHeight * 0.63,
+  },
   container: {
     flex: 1,
     flexDirection: 'column',
     backgroundColor: '#ffffff',
   },
   preview: {
-    flex: 1,
+    flex: 2,
     justifyContent: 'flex-end',
     alignItems: 'center',
   },
