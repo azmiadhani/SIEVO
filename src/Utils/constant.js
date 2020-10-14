367917660cbfd381586730c75edce2e25c3bb298
► Azmi Adhani <azmi.adhani@ulm.ac.id>
2020-10-15 05:57:07 +0800

    Url fix

--- src/Utils/constant.js
+++ src/Utils/constant.js
@@ -1,7 +1,11 @@
 export const WARNA_UTAMA = '#FFFFFF';
 export const WARNA_DISABLE = '#3E3E3E';
 export const APP_VERSION = '1.0.0-beta';
+
+// Login
 export const URL_API_LOGIN = 'https://api.ulm.ac.id:8081/';
-export const URL_API_MAINAPP = 'http://10.72.2.28/pemilu-m/';
-export const URL_PATH_FOTO_KANDIDAT =
-  'http://10.72.2.28/pemilu-m/assets/images/';
+
+// Main App
+export const URL_DOMAIN = 'http://192.168.1.5/';
+export const URL_API_MAINAPP = URL_DOMAIN + 'pemilu-m/';
+export const URL_PATH_FOTO_KANDIDAT = URL_DOMAIN + 'pemilu-m/assets/images/';
