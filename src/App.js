ac46c2ef99258031a24693e253cc30f71f9b4e52
► Azmi Adhani <azmi.adhani@ulm.ac.id>
2020-10-19 17:29:52 +0800

    Notifikasi Sudah Masuk Checkpoint

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useEffect} from 'react';
+import {StyleSheet, Text, View, Alert} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import Router from './router';
-
+import messaging from '@react-native-firebase/messaging';
 const App = () => {
+  useEffect(() => {
+    messaging()
+      .getToken()
+      .then((token) => {
+        console.log(token);
+      });
+
+    const unsubscribe = messaging().onMessage(async (remoteMessage) => {
+      Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+
+    return unsubscribe;
+  }, []);
   return (
     <NavigationContainer>
       <Router />
