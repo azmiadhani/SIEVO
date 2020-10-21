526e6ceb283d8cb9666a6f1e6912248f4df0b2c0
► Azmi Adhani <azmi.adhani@ulm.ac.id>
2020-10-21 16:51:57 +0800

    update url json

--- src/Utils/constant.js
+++ src/Utils/constant.js
@@ -8,4 +8,4 @@ export const URL_DOMAIN = 'http://10.72.2.41/pemilu-m/';
 
 // URL json api for base_url
 export const URL_JSON_API =
-  'https://firebasestorage.googleapis.com/v0/b/pemilu-m.appspot.com/o/url.json?alt=media&token=badb6708-0407-4b06-9b41-3e6200e3641e';
+  'https://firebasestorage.googleapis.com/v0/b/pemilu-m.appspot.com/o/url.json?alt=media&token=1be59665-456f-41dd-88a2-0e8743327a93';
